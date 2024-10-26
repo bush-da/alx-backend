2e4227569e4417dcdc7345e0751941d9ec327ffa
Daniel Tujuma <danielsevani12@gmail.com>
2024-10-26 22:39:14 +0300

    function return first and last index

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Script with function index_range that which accept two parameters
+start page and page size and returns start and end index of the page
+"""
+
+
+def index_range(page, page_size):
+    end_idx = page_size * page
+    start_idx = end_idx - page_size
+    return (start_idx, end_idx)
