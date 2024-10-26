98c3bfb80365184c1f0302b34f2582f642d9ad01
Daniel Tujuma <danielsevani12@gmail.com>
2024-10-26 22:42:42 +0300

    add parameter types

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -4,7 +4,9 @@ start page and page size and returns start and end index of the page
 """
 
 
-def index_range(page: int, page_size: int):
+def index_range(page: int, page_size: int) -> tuple:
+    """Function that accept page number and page size
+    return start and end index of page"""
     end_idx = page_size * page
     start_idx = end_idx - page_size
     return (start_idx, end_idx)
