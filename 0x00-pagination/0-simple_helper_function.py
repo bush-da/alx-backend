48e43b8b16c6a2d67151dab715191d99e4c33a06
Daniel Tujuma <danielsevani12@gmail.com>
2024-10-26 22:40:47 +0300

    add parameter types

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -4,7 +4,7 @@ start page and page size and returns start and end index of the page
 """
 
 
-def index_range(page, page_size):
+def index_range(page: int, page_size: int):
     end_idx = page_size * page
     start_idx = end_idx - page_size
     return (start_idx, end_idx)
