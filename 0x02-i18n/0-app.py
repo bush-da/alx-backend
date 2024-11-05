6f4c929e62cb98bba877d1ce4373b216082c76e4
Daniel Tujuma <danielsevani12@gmail.com>
2024-11-05 22:34:15 +0300

    fix minor issue

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -2,12 +2,16 @@
 """Flask module to server the static page"""
 from flask import Flask, render_template
 
+
 app = Flask(__name__)
+app.url_map.strict_slashes = False
+
 
 @app.route('/')
 def home():
     """home page"""
     return render_template('0-index.html')
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
