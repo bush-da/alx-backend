4dd6076b6e544a464ec4c06cf88b13765af2ba05
Daniel Tujuma <danielsevani12@gmail.com>
2024-11-05 22:46:58 +0300

    update with babel

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""Flask module to server the static page"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+class Config:
+    """config class for app available languages"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+babel = Babel()
+
+@app.route('/')
+def home():
+    """home page"""
+    return render_template('0-index.html')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
