221d166ec01bcdef007699b708cde35b3959c970
Daniel Tujuma <danielsevani12@gmail.com>
2024-11-06 02:33:04 +0300

    check if it exists if header request

--- 0x02-i18n/6-app.py
+++ 0x02-i18n/6-app.py
@@ -54,6 +54,10 @@ def get_locale() -> str:
     if g.user and g.user.get('locale') in app.config('LANGUAGES'):
         return g.user.get('locale')
 
+    header_locale =request.header.get('locale', '')
+    if header_locale in app.config['LANGUAGES']:
+        return header_locale
+
     return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
