64dcc3344c763bc39d2c4f33d74c0267da7adf51
Daniel Tujuma <danielsevani12@gmail.com>
2024-11-05 23:30:18 +0300

    ignore

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""Flask module to server the static page"""
+from flask import Flask, render_template
+from flask_babel import Babel, gettext
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+class Config:
+    """config class for app available languages"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+babel = Babel()
+
+
+@babel.localeselector
+def get_locale():
+    """determine which the match with supported language"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def home():
+    """home page"""
+    home_header = gettext("Hello, World!")
+    home_title = gettext("Welcome to Holberton")
+    return render_template('3-index.html', home_header=home_header, home_title=home_title)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
