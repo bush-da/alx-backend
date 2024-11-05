f138bbfac49978a2806fe7b2bf4199b4120b7f23
Daniel Tujuma <danielsevani12@gmail.com>
2024-11-06 02:12:53 +0300

    mock logging in

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -2,7 +2,8 @@
 """A Basic Flask app with internationalization support.
 """
 from flask_babel import Babel
-from flask import Flask, render_template, request
+from typing import Union, Dict
+from flask import Flask, render_template, request, g
 
 
 class Config:
@@ -17,9 +18,6 @@ app = Flask(__name__)
 app.config.from_object(Config)
 app.url_map.strict_slashes = False
 babel = Babel(app)
-
-
-#Mocked user data
 users = {
     1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
     2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
@@ -28,36 +26,38 @@ users = {
 }
 
 
-def get_user():
-    """Define a function that return a user dictionary"""
-    user_id = request.args.get('login_as')
-    if user_id and user_id.isdigit():
-        return users.get(int(user_id))
+def get_user() -> Union[Dict, None]:
+    """Retrieves a user based on a user id.
+    """
+    login_id = request.args.get('login_as')
+    if login_id:
+        return users.get(int(login_id))
     return None
 
 
 @app.before_request
-def before_request():
-    """Executed before each request"""
-    g.user = get_user()
+def before_request() -> None:
+    """Performs some routines before each request's resolution.
+    """
+    user = get_user()
+    g.user = user
 
 
 @babel.localeselector
 def get_locale() -> str:
-    """Determine the best match for supported languages."""
-    # Check if 'locale' parameter exists in the request arguments
-    locale = request.args.get('locale')
-    if locale in app.config['LANGUAGES']:
+    """Retrieves the locale for a web page.
+    """
+    locale = request.args.get('locale', '')
+    if locale in app.config["LANGUAGES"]:
         return locale
-    # Fallback to `accept_languages`
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
 @app.route('/')
 def get_index() -> str:
     """The home/index page.
     """
-    return render_template('4-index.html')
+    return render_template('5-index.html')
 
 
 if __name__ == '__main__':
