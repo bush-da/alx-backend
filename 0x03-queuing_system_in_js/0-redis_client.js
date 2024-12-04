9a18b4ee6a147b31649bc13eeef27b34106b3f7d
Daniel Tujuma <danielsevani12@gmail.com>
2024-12-04 21:37:16 +0300

    node redis client

---
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,13 @@
+import { createClient } from 'redis';
+
+// Create a Redis client
+const client = createClient();
+
+// Handle connection events
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.error(`Redis client not connected to the server: ${err.message}`);
+});
